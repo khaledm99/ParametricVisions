65fc69278af2dfaabbc95f0565299cec545c4493
khaled <khaledmog99@gmail.com>
2024-03-19 10:18:35 -0600

    initial commit, CMakeLists.txt from cpsc589 a2, including libraries.

---
+++ build/CMakeFiles/visions.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visions.dir/include/imgui-docking-1.90.5/imgui.cpp.o"
+  "CMakeFiles/visions.dir/include/imgui-docking-1.90.5/imgui.cpp.o.d"
+  "CMakeFiles/visions.dir/include/imgui-docking-1.90.5/imgui_demo.cpp.o"
+  "CMakeFiles/visions.dir/include/imgui-docking-1.90.5/imgui_demo.cpp.o.d"
+  "CMakeFiles/visions.dir/include/imgui-docking-1.90.5/imgui_draw.cpp.o"
+  "CMakeFiles/visions.dir/include/imgui-docking-1.90.5/imgui_draw.cpp.o.d"
+  "CMakeFiles/visions.dir/include/imgui-docking-1.90.5/imgui_tables.cpp.o"
+  "CMakeFiles/visions.dir/include/imgui-docking-1.90.5/imgui_tables.cpp.o.d"
+  "CMakeFiles/visions.dir/include/imgui-docking-1.90.5/imgui_widgets.cpp.o"
+  "CMakeFiles/visions.dir/include/imgui-docking-1.90.5/imgui_widgets.cpp.o.d"
+  "CMakeFiles/visions.dir/src/main.cpp.o"
+  "CMakeFiles/visions.dir/src/main.cpp.o.d"
+  "visions"
+  "visions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/visions.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for visions.
+# This may be replaced when dependencies are built.
